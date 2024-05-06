945b25d2cfbb67ef14e74137743ea735b1df0bd3
khant-min <kmin01405@gmail.com>
2024-05-06 15:30:15 +0630

    feat: add register and login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./components/common/MainLayout";
 import Home from "./pages/Home";
 import "./App.css";
+import Register from "./pages/register";
+import Login from "./pages/login";
 
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout />,
-
       children: [
         {
           index: true,
@@ -20,6 +21,14 @@ const App = () => {
           element: <div>about</div>,
         },
       ],
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
     },
   ]);
   return <RouterProvider router={router} />;
